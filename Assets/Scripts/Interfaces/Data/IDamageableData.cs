7c99b781c038450f0774c0cfdc7277881b9dad44
pe4ceduk3 <sdamirbekov@gmail.com>
2026-07-03 16:39:01 +0600

    feat: add flag canTakeDamage

--- Assets/Scripts/Interfaces/Data/IDamageableData.cs
+++ Assets/Scripts/Interfaces/Data/IDamageableData.cs
@@ -2,6 +2,7 @@ namespace Interfaces.Data
 {
     public interface IDamageable
     {
+        bool CanTakeDamage { get; set; }
         float Damage { get; set; }
         float Health { get; set; }
         float MaxHealth { get; set; }
