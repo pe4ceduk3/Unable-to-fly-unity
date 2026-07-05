5f648467877884b3ffa9666b99fc0bbd970282b3
pe4ceduk3 <sdamirbekov@gmail.com>
2026-07-05 21:17:33 +0600

    refactor: ballistic math and romove unused logic

--- Assets/Scripts/Components/Movement/BallisticsComponent.cs
+++ Assets/Scripts/Components/Movement/BallisticsComponent.cs
@@ -8,15 +8,13 @@ namespace Components.Movement
     {
         public void SetStartVelocity(IBallisticsData data, Rigidbody2D body)
         {
-            float velocityY = (2.0f * data.Peak) / data.Duration;
+            float velocityY = (2.0f * data.Peak) / data.Duration * Time.fixedDeltaTime;
             body.linearVelocity = new Vector2(body.linearVelocity.x, velocityY);
         }
-        public void ApplyGravity(IBallisticsData data, IGravityData gravityData, Rigidbody2D body)
+        public void ApplyGravity(IBallisticsData data, Rigidbody2D body)
         {
-            float gravity = (-2.0f * data.Peak) / (data.Duration * data.Duration);
-            float velocityY = gravity * Time.fixedDeltaTime;
-            if (gravity > gravityData.MaxGravity) gravity = gravityData.MaxGravity;
-            body.linearVelocity += new Vector2(body.linearVelocity.x, velocityY);
+            float gravity = (-2.0f * data.Peak) / (data.Duration * data.Duration) * Time.fixedDeltaTime;
+            body.linearVelocity += new Vector2(body.linearVelocity.x, gravity);
         }
     }
 }
